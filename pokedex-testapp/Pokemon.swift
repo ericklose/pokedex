859ba9942ca17b8b79e47d6cf863ec4212015478
Eric Klose <ericklose@gmail.com>
2016-02-25 15:20:39 -0500

    second video

---
+++ pokedex-testapp/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  pokedex-testapp
+//
+//  Created by Eric Klose on 2/25/16.
+//  Copyright © 2016 Eric Klose. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokeId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
